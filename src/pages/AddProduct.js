8e798f8d5b452518f746cd5321426ac12ba160a8
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-06-25 10:10:21 +0530

    S

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -2,15 +2,50 @@ import React from 'react';
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import Button from '@material-ui/core/Button';
 import Footer from '../components/Footer';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 const AddProduct = () => {
-
+    const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const [ data, setData ] = React.useState('Not Found');
 
+    React.useEffect(() => {
+        handleClickOpen();
+    }, [data]);
+
     return (
         <>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">Bar Code Sample Test</DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Your Data is {data}
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                    Close
+                </Button>
+                </DialogActions>
+            </Dialog>
+
             <div className="container mt-4">
                 <h2 className="fw-bold">
                     Shelve 12
